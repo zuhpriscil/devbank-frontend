2a056491bbc55dc5c34a7d6fa4c4ef3526779760
MEH ENGELBERT KUM <menbongtah@gmail.com>
2026-02-18 21:49:51 +0100

    Update UI text

---
+++ devbank-frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h1>DevBank Frontend</h1>
+      <p>Deployed on AWS ECS via Application Load Balancer</p>
+      <p>CI/CD test change</p>
+    </div>
+  );
+}
+
+export default App; 
